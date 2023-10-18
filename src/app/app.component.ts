50064a4fa4e92fe4489581e7cf29d4814541f091
Maxime MEDAL <medal.max@gmail.com>
2023-10-18 22:58:19 +0200

    73 - list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, delay, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ANGULAR-INTERVIEW';
+export class AppComponent implements OnInit {
+  bookList$: Observable<string[]> = of(['📙', '📕', '📗', '📘']).pipe(delay(1000));
+  books: string[] = [];
+
+  ngOnInit(): void {
+    this.getBooks();
+  }
+
+  getBooks() {
+    this.bookList$.subscribe(books => {
+      this.books = books;
+    })
+  }
 }
